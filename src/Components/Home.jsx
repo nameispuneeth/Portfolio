417ba819f434eaa8e775eaceefcc6c7a5765919a
nameispuneeth <puneethkumarkaramala@gmail.com>
2025-10-26 22:12:44 +0530

    Adding Projects

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,6 +6,7 @@ export default function Home() {
     const terminalRef = useRef(null);
 
     const [commandRunning, setCommandRunning] = useState(false);
+    const [projectsFolderOpen,setprojectsFolderOpen]=useState(false);
 
     const [fullScreen, setFullScreen] = useState(false);
     const date = new Date().toUTCString();
@@ -15,10 +16,9 @@ export default function Home() {
     commands.set("nameiz", `\n\t---------------------\n\t|\t\t\t\t\t|\n\t|\tPuneeth Kumar\t|\n\t|\t\t\t\t\t|\n------------------------------\n`)
     commands.set("whoami", "Hey👋! I'm <b class='text-red-600'>Puneeth Kumar</b>, a passionate software developer currently pursuing my 3rd year in Computer Science Engineering at RGUKT-RKV. \nI’ve always loved messing around with code and spending late nights debugging it. I also love to solve coding problems on platforms like LeetCode and GeeksforGeeks and spending a hell of a time fixing bugs and tle issues, and have earned multiple badges along the way.\nI’m always eager to learn something new and build projects that are actually useful.")
     commands.set("edu-logs",
-
-        "<p class='text-base font-extrabold'>Education</p>" +
+        "<br/><p class='text-base font-extrabold'>Education</p>" +
         "----------<br>" +
-        "<b>B.Tech</b> :Computer Science Engineering - RGUKT-RK Valley (2023 - Present) - 8.35 CGPA\n" +
+        "<b>B.Tech</b> : Computer Science Engineering - RGUKT-RK Valley (2023 - Present) - 8.35 CGPA\n" +
         "<b>PUC</b> : RGUKT-RK Valley (2021 - 2023) - 9.7 CGPA<br>" +
         "<b>Secondary Schooling</b> : Basavaraja Govt. High School (2016 - 2021) - 10.0 CGPA<br>" +
         "\nup to date, audited 3 degrees in 2s\n" +
@@ -27,7 +27,6 @@ export default function Home() {
 
     commands.set("git contact",
         ">>> Accessing contact directory......\n" +
-
         "Email: <a href='mailto:puneethkumarkaramala@gmail.com' class='text-blue-400 underline'>puneethkumarkaramala@gmail.com</a><br>" +
         "Phone: +917416558950<br>" +
         "GitHub: <a href='https://github.com/nameispuneeth' target='_blank' class='text-blue-400 underline'>nameispuneeth</a><br/>" +
@@ -42,10 +41,10 @@ export default function Home() {
         "Coding Ninjas: <a href='https://www.naukri.com/code360/profile/puneethkumar' target='  _blank' class='text-blue-400 underline'>puneethkumar</a><br/>" +
         "Hacker Rank: <a href='https://www.hackerrank.com/profile/puneeth0121' target='  _blank' class='text-blue-400 underline'>puneeth0121</a><br/>" +
         ">>> Coding Profiles Fetched in <b class='text-green-500'>2580ms</b>."
-
-
     );
-    useEffect(() => {
+    commands.set("cd projects",">>> Switching To Projects Directory.....<br/>"+`> Located 4 Repositories : <br/>\t1.CodeBite<br/>\t2.Civiq<br>\t3.GPA Calculator<br/>\t4.Currency Convertor<br/>`+">>> Run Project Name For Details");
+ commands.set("codebite","\t----------------------\n"+"\t CodeBite - OnlineIDE\n"+"\t----------------------\n"+"\t CodeBite Is An OnlineIDE with AI Integrated, where user can run code in browser directly without installing any package.It features AI integration for coding assistance and supports saving, updating, and deleting your files.It is build for the speed and simplicity.No Downloads,No Setup,just code and run.\n"+"Live Demo : <a href='https://codebite-onlineide-puneeth.vercel.app' target='_blank' class='text-blue-400 underline'>CodeBite</a>\n"+"Features : \n\t> Real Time Code Execution With I/O Operations.\n\t> Supports Upto 7 Languages.\n\t> AI-Powered Coding Assistance.\n\t> Save,Update,Download and Delete Files.\n\t> User authentication & security using JWT and bcrypt\n\t> Clean & Minimal Interface\n"+"TechStack : \n\t Frontend : ReactJS,TailWind CSS \n\t Backend : Node.js,Express.js\n\t Authentication and Email : JWT,Bcrypt,NodeMailer \n\t DataBase : MongoDB \n\t Code Execution : Judge0 API")
+            useEffect(() => {
         if (terminalRef.current) {
           terminalRef.current.scrollTop = terminalRef.current.scrollHeight;
         }
